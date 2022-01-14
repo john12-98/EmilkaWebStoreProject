64fab0a18b9a94f6180e85ae29d72e2109062621
Yohannes Taffese <yohannestaffese12@gmail.com>
2022-01-13 21:57:06 -0800

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Slider from "../components/Slider";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <Slider />
+    </div>
+  );
+};
+
+export default Home;
